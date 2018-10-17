0c3fbc357aba81a9efd73fde83a873bb33ff1209
omurbekjk <omurbek.jk@gmail.com>
2018-10-17 20:06:48 +0600

    reset password activity added

---
+++ app/src/main/java/io/jachoteam/kaska/ResetPasswordActivity.java
@@ -0,0 +1,61 @@
+package io.jachoteam.kaska;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class ResetPasswordActivity extends AppCompatActivity {
+    private FirebaseAuth auth;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reset_password);
+        Log.i("RESET_PASSWORD", "LAUNCHED");
+        auth = FirebaseAuth.getInstance();
+        Button resetButton = (Button) findViewById(R.id.reset_button);
+        final EditText editText = (EditText) findViewById(R.id.email_input);
+        final ProgressBar progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        resetButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO validate email and send reset link
+                Log.i(editText.getText().toString(), "RESET_LINK");
+
+                String email = editText.getText().toString().trim();
+
+                if (TextUtils.isEmpty(email)) {
+                    Toast.makeText(getApplication(), "Enter your registered email id", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                progressBar.setVisibility(View.VISIBLE);
+
+                auth.sendPasswordResetEmail(email)
+
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(ResetPasswordActivity.this, "We have sent you instructions to reset your password!", Toast.LENGTH_SHORT).show();
+                                } else {
+                                    Toast.makeText(ResetPasswordActivity.this, "Failed to send reset email!", Toast.LENGTH_SHORT).show();
+                                }
+                                progressBar.setVisibility(View.GONE);
+                            }
+                        });
+            }
+        });
+
+    }
+}
