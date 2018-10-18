f0531c673154282bf696d7e60ab8cddc06b065be
omurbekjk <omurbek.jk@gmail.com>
2018-10-18 20:17:29 +0600

    list people on discover page

--- app/src/main/java/io/jachoteam/kaska/ProfileViewActivity.java
+++ app/src/main/java/io/jachoteam/kaska/ProfileViewActivity.java
@@ -9,8 +9,18 @@ import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import io.jachoteam.kaska.models.User;
+
 public class ProfileViewActivity extends AppCompatActivity {
     String TAG = "ProfileViewActivity";
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference ref;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,6 +31,22 @@ public class ProfileViewActivity extends AppCompatActivity {
         String uid = intent.getStringExtra("uid");
         String username = intent.getStringExtra("username");
         Log.d(TAG, uid+": " + username);
+
+        ref = database.getReference("users/"+uid);
+        // Attach a listener to read the data at our posts reference
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                User user = dataSnapshot.getValue(User.class);
+                System.out.println(user);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                System.out.println("The read failed: " + databaseError.getCode());
+            }
+        });
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
